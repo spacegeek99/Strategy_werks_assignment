350e7c14af8c64935caf468b3f3ac07db0d5bbd5
spacegeek99 <jatinkumar.singh1998@gmail.com>
2024-08-21 20:51:36 +0530

    feat: build successful

--- Strategy_werks/src/main.tsx
+++ Strategy_werks/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 import { Provider } from 'react-redux'
-import store from './state/store.ts'
+import store from './state/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
